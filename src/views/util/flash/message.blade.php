9b0f01aa47024b6f68fb77643429ced44fcc078c
Elias da Rosa <elias@eliasdarosa.com.br>
2016-04-24 08:37:15 -0300

    Adicionado as views customizadas do Laracast/Flash

---
+++ src/views/util/flash/message.blade.php
@@ -0,0 +1,24 @@
+@if (session()->has('flash_notification.message'))
+    @if (session()->has('flash_notification.overlay'))
+        @include('BW::util.flash.modal', [
+            'modalClass' => 'flash-modal',
+            'title'      => session('flash_notification.title'),
+            'body'       => session('flash_notification.message')
+        ])
+    @else
+        <div class="alert alert-{{ session('flash_notification.level') }}">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+
+            {!! session('flash_notification.message') !!}
+
+            @if ($errors->has())
+                <ul style="margin-top: 15px;">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            @endif
+        </div>
+    @endif
+@endif
+
