9a52a851cc361000e0afa3175dfdff04607cbbcf
Elias da Rosa <elias@eliasdarosa.com.br>
2016-04-27 22:47:02 -0300

    Corrigido menu lateral, ativa menu nas urls '/{id}/edit' e '/create'

--- src/public/vendor/sb-admin/dist/js/sb-admin-2.js
+++ src/public/vendor/sb-admin/dist/js/sb-admin-2.js
@@ -1,16 +1,16 @@
-$(function() {
-
-    $('#side-menu').metisMenu();
-
-});
-
 //Loads the correct sidebar on window load,
 //collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
 $(function() {
+
+    //
+    $('#side-menu').metisMenu();
+
+    //
     $(window).bind("load resize", function() {
         topOffset = 50;
         width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+
         if (width < 768) {
             $('div.navbar-collapse').addClass('collapse');
             topOffset = 100; // 2-row-menu
@@ -20,17 +20,24 @@ $(function() {
 
         height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
         height = height - topOffset;
-        if (height < 1) height = 1;
+
+        if (height < 1) {
+            height = 1;
+        }
+
         if (height > topOffset) {
             $("#page-wrapper").css("min-height", (height) + "px");
         }
     });
 
-    var url = window.location;
+    //
+    var url = window.location.href;
+    var regexp = /(\/create|\/[1-9]+\/edit)$/i;
     var element = $('#side-menu a').filter(function() {
-        return this.href == url;
-    }).addClass('active').parent().parent().addClass('in').parent();
+        return  this.href == url || this.href == url.replace(regexp, '');
+    }).addClass('active').parent().parent().addClass('in');
 
+    //
     if (element.is('li')) {
         element.addClass('active');
     }
