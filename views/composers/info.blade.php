178ce0d266e4367d2ef280c09d7d094f8afcfbbb
Elias da Rosa <elias@eliasdarosa.com.br>
2016-05-26 23:45:53 -0300

    Removido o menu de configurações

--- views/composers/info.blade.php
+++ views/composers/info.blade.php
@@ -2,14 +2,6 @@
     <li>
         <a href="#"><i class="fa fa-user fa-fw"></i> {{ $email }}</a>
     </li>
-    <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-gear fa-fw"></i> Configurações <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-            <li>
-                <a href="{{ route('bw.users.index') }}"><i class="fa fa-user fa-fw"></i> Gerenciar usuários</a>
-            </li>
-        </ul>
-    </li>
     <li>
         <a href="{{ route('bw.logout') }}"><i class="fa fa-sign-out fa-fw"></i> Sair</a>
     </li>
