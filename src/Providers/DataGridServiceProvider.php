fed2e8dbc07445b8db5390ffddb92364bff4adfa
Elias da Rosa <elias@eliasdarosa.com.br>
2016-05-30 21:28:07 -0300

    Removidos arquivos publicados desnecessários do vendo zofe/rapyd

--- src/Providers/DataGridServiceProvider.php
+++ src/Providers/DataGridServiceProvider.php
@@ -2,13 +2,52 @@
 
 namespace BW\Providers;
 
-use Illuminate\Support\ServiceProvider as ServiceProvider;
+use Zofe\Rapyd\Rapyd;
+use Collective\Html\FormBuilder;
+use Collective\Html\HtmlBuilder;
+use Zofe\Rapyd\RapydServiceProvider;
 
-class DataGridServiceProvider extends ServiceProvider
+class DataGridServiceProvider extends RapydServiceProvider
 {
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $dir = base_path('vendor/zofe/rapyd/src');
+        $this->loadViewsFrom($dir.'/../views', 'rapyd');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
     public function register()
     {
-        // Register service provider
-        $this->app->register('Zofe\Rapyd\RapydServiceProvider');
+        $this->app->register('Collective\Html\HtmlServiceProvider');
+
+        Rapyd::setContainer($this->app);
+
+        $this->app->booting(function () {
+            $loader  =  \Illuminate\Foundation\AliasLoader::getInstance();
+            $loader->alias('Rapyd', 'Zofe\Rapyd\Facades\Rapyd'     );
+            $loader->alias('DataFilter', 'Zofe\Rapyd\Facades\DataFilter');
+        });
     }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+
 }
+
+
