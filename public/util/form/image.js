24b6cd19bd47e53472ed9bdeb9325cba41b1f146
Elias da Rosa <elias@eliasdarosa.com.br>
2017-02-14 16:47:36 -0200

    Removido arquivos

--- public/util/form/image.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $('form .field-image .btn-remove').on('click', function() {
-        if(!confirm('Tem certeza que deseja remover esta imagem?')){
-            return false;
-        }
-    });
-});
