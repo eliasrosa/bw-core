9f7f8bc3ac90ce204fe12acf9a30db31e6b52bdc
Elias da Rosa <elias@eliasdarosa.com.br>
2017-04-26 11:17:05 -0300

    Galeria só será exibida na edição do registro

--- views/util/relationships/image/gallery.blade.php
+++ views/util/relationships/image/gallery.blade.php
@@ -9,38 +9,40 @@
              data-url-upload="{{ route('bw.relationships.image.gallery.upload') }}"
              class="field-gallery clearfix">
 
-            <span class="btn btn-success btn-open-gallery-images">Abrir {{ $item->label }}</span>
 
-            <div class="modal fade" id="modal-galeria-imagens" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog">
-                <div class="modal-dialog modal-lg">
-                    <div class="modal-content">
-                        
-                        <div class="modal-header">
-                            <h4 class="title">{{ $item->label }}</h4>
-                        </div>
+            @if($item->getRefId() > 0)
+                <span class="btn btn-success btn-open-gallery-images">Abrir {{ $item->label }}</span>
 
-                        <div class="modal-body">
-                            <div class="loading">
-                                <span class="fa fa-circle-o-notch fa-spin"></span>
-                                Carregando...
+                <div class="modal fade" id="modal-galeria-imagens" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog">
+                    <div class="modal-dialog modal-lg">
+                        <div class="modal-content">
+                            
+                            <div class="modal-header">
+                                <h4 class="title">{{ $item->label }}</h4>
                             </div>
-                            <div class="empty">Nenhuma imagem encontrada!</div>
-                        </div>
 
-                        <div class="modal-footer">
+                            <div class="modal-body">
+                                <div class="loading">
+                                    <span class="fa fa-circle-o-notch fa-spin"></span>
+                                    Carregando...
+                                </div>
+                                <div class="empty">Nenhuma imagem encontrada!</div>
+                            </div>
 
-                            <span class="btn btn-success fileinput-button">
-                                <i class="glyphicon glyphicon-plus"></i>
-                                <span>Anexar imagens...</span>
-                                <input type="file" name="imagem" multiple>
-                            </span>
+                            <div class="modal-footer">
 
-                            <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+                                <span class="btn btn-success fileinput-button">
+                                    <i class="glyphicon glyphicon-plus"></i>
+                                    <span>Anexar imagens...</span>
+                                    <input type="file" name="imagem" multiple>
+                                </span>
+
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-
+            @endif
         </div>
     </div>
 
